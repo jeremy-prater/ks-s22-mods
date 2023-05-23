e47571f370996d15f727fe1dcd457671ee291f7c
Jeremy Prater <silverevo03@gmail.com>
2023-05-23 09:11:52 -0700

    - LVGL works

--- s22_display/main/s22_display.c
+++ s22_display/main/s22_display.c
@@ -1,86 +1,143 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+/* LVGL Example project
  *
- * SPDX-License-Identifier: CC0-1.0
+ * Basic project to test LVGL on ESP32 based projects.
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
  */
-
+#include <stdbool.h>
 #include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
+#include <stdlib.h>
+#include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_err.h"
-#include "esp_log.h"
-#include "st7789.h"
+#include "esp_freertos_hooks.h"
+#include "freertos/semphr.h"
+#include "esp_system.h"
 #include "driver/gpio.h"
 
-#define ST7789_GPIO_MOSI GPIO_NUM_23
-#define ST7789_GPIO_SCLK GPIO_NUM_18
-#define ST7789_GPIO_CS GPIO_NUM_19
-#define ST7789_GPIO_DC GPIO_NUM_22
-#define ST7789_GPIO_RESET GPIO_NUM_19
-#define ST7789_GPIO_BACKLIGHT GPIO_NUM_19
+#include "lvgl.h"
+#include "lvgl_helpers.h"
 
-#define ST7789_SPI_HOST VSPI_HOST
-#define ST7789_DMA_CHAN 2
-#define ST7789_DISPLAY_WIDTH 240
-#define ST7789_DISPLAY_HEIGHT 240
-#define ST7789_BUFFER_SIZE 20
+#define TAG "s22-main"
+#define LV_TICK_PERIOD_MS 1
 
-static const char *TAG = "main";
+static void lv_tick_task(void *arg);
+static void guiTask(void *pvParameter);
+static void create_ui_application(void);
 
-void app_main(void)
-{
-    ESP_LOGI(TAG, "Hello world!");
+void app_main() {
 
-    st7789_driver_t display = {
-        .pin_mosi = ST7789_GPIO_MOSI,
-        .pin_sclk = ST7789_GPIO_SCLK,
-        .pin_cs = ST7789_GPIO_CS,
-        .pin_dc = ST7789_GPIO_DC,
-        .pin_reset = ST7789_GPIO_RESET,
-        .pin_backlight = ST7789_GPIO_BACKLIGHT,
-        .spi_host = ST7789_SPI_HOST,
-        .dma_chan = ST7789_DMA_CHAN,
-        .display_width = ST7789_DISPLAY_WIDTH,
-        .display_height = ST7789_DISPLAY_HEIGHT,
-        .buffer_size = ST7789_BUFFER_SIZE * ST7789_DISPLAY_WIDTH, // 2 buffers with 20 lines
+    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
+     * Otherwise there can be problem such as memory corruption and so on.
+     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+}
+
+/* Creates a semaphore to handle concurrent call to lvgl stuff
+ * If you wish to call *any* lvgl function from other threads/tasks
+ * you should lock on the very same semaphore! */
+SemaphoreHandle_t xGuiSemaphore;
+
+static void guiTask(void *pvParameter) {
+
+    (void) pvParameter;
+    xGuiSemaphore = xSemaphoreCreateMutex();
+
+    lv_init();
+
+    /* Initialize SPI or I2C bus used by the drivers */
+    lvgl_driver_init();
+
+    lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf1 != NULL);
+
+    lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf2 != NULL);
+
+    static lv_disp_buf_t disp_buf;
+
+    uint32_t size_in_px = DISP_BUF_SIZE;
+
+    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = disp_driver_flush;
+
+#if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT || defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
+    disp_drv.rotated = 1;
+#endif
+
+    disp_drv.buffer = &disp_buf;
+    lv_disp_drv_register(&disp_drv);
+
+     /* Create and start a periodic timer interrupt to call lv_tick_inc */
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &lv_tick_task,
+        .name = "periodic_gui"
     };
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
-    ESP_ERROR_CHECK(st7789_init(&display));
+    /* Create the demo application */
+    create_ui_application();
 
-    st7789_reset(&display);
-    st7789_lcd_init(&display);
+    while (1) {
+        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
+        vTaskDelay(pdMS_TO_TICKS(10));
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    ESP_LOGI(TAG, "This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
-             CONFIG_IDF_TARGET,
-             chip_info.cores,
-             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-             (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
-             (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    ESP_LOGI(TAG, "silicon revision v%d.%d, ", major_rev, minor_rev);
-    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
-    {
-        ESP_LOGI(TAG, "Get flash size failed");
-        return;
+        /* Try to take the semaphore, call lvgl related function on success */
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+            lv_task_handler();
+            xSemaphoreGive(xGuiSemaphore);
+       }
     }
 
-    ESP_LOGI(TAG, "%" PRIu32 "MB %s flash", flash_size / (uint32_t)(1024 * 1024),
-             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    /* A task should NEVER return */
+    free(buf1);
+    free(buf2);
+    vTaskDelete(NULL);
+}
 
-    ESP_LOGI(TAG, "Minimum free heap size: %" PRIu32 " bytes", esp_get_minimum_free_heap_size());
 
-    while (true)
-    {
-        
+static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
+{
+    if(event == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, NULL);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
     }
+}
+
+static void create_ui_application(void)
+{
+    lv_obj_t * btn = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    // lv_obj_set_event_cb(btn, btn_event_cb);                 /*Assign a callback to the button*/
+    lv_obj_t * label = lv_label_create(btn, NULL);          /*Add a label to the button*/
+    lv_label_set_text(label, "Yeah");                     /*Set the labels text*/
+
+    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
+    lv_obj_set_pos(btn2, 150, 10);                            /*Set its position*/
+    lv_obj_set_size(btn2, 120, 50);                          /*Set its size*/
+    lv_obj_t * label2 = lv_label_create(btn2, NULL);          /*Add a label to the button*/
+    lv_label_set_text(label2, "Buddy");                     /*Set the labels text*/
+
+
+}
+
+static void lv_tick_task(void *arg) {
+    (void) arg;
+
+    lv_tick_inc(LV_TICK_PERIOD_MS);
 }
