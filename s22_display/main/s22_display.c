f23e4ebd976e18ebf6ad189dc02066f5e8b9adae
Jeremy Prater <silverevo03@gmail.com>
2023-05-28 02:39:34 -0700

    - Lvgl 8, Idf 5, st7789 yeeah

--- s22_display/main/s22_display.c
+++ s22_display/main/s22_display.c
@@ -18,10 +18,17 @@
 #include "esp_freertos_hooks.h"
 #include "freertos/semphr.h"
 #include "esp_system.h"
+#include "esp_timer.h"
 #include "driver/gpio.h"
 
 #include "lvgl.h"
 #include "lvgl_helpers.h"
+
+#if LV_USE_DEMO_WIDGETS
+#include "widgets/lv_demo_widgets.h"
+#else
+#include "ui/ui.h"
+#endif
 
 #define TAG "s22-main"
 #define LV_TICK_PERIOD_MS 1
@@ -30,12 +37,13 @@ static void lv_tick_task(void *arg);
 static void guiTask(void *pvParameter);
 static void create_ui_application(void);
 
-void app_main() {
+void app_main()
+{
 
     /* If you want to use a task to create the graphic, you NEED to create a Pinned task
      * Otherwise there can be problem such as memory corruption and so on.
      * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
-    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096 * 2, NULL, 0, NULL, 1);
 }
 
 /* Creates a semaphore to handle concurrent call to lvgl stuff
@@ -43,9 +51,10 @@ void app_main() {
  * you should lock on the very same semaphore! */
 SemaphoreHandle_t xGuiSemaphore;
 
-static void guiTask(void *pvParameter) {
+static void guiTask(void *pvParameter)
+{
 
-    (void) pvParameter;
+    (void)pvParameter;
     xGuiSemaphore = xSemaphoreCreateMutex();
 
     lv_init();
@@ -53,50 +62,59 @@ static void guiTask(void *pvParameter) {
     /* Initialize SPI or I2C bus used by the drivers */
     lvgl_driver_init();
 
-    lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    lv_color_t *buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf1 != NULL);
 
-    lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    lv_color_t *buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf2 != NULL);
 
-    static lv_disp_buf_t disp_buf;
+    static lv_disp_draw_buf_t disp_buf;
 
     uint32_t size_in_px = DISP_BUF_SIZE;
 
-    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
 
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;
 
-#if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT || defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
-    disp_drv.rotated = 1;
+    disp_drv.draw_buf = &disp_buf;
+    lv_disp_t *display = lv_disp_drv_register(&disp_drv);
+
+#if defined CONFIG_DISPLAY_ORIENTATION_LANDSCAPE
+    lv_disp_set_rotation(display, LV_DISP_ROT_90);
+#elif defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
+    lv_disp_set_rotation(display, LV_DISP_ROT_180);
+#elif defined CONFIG_DISPLAY_ORIENTATION_LANDSCAPE_INVERTED
+    lv_disp_set_rotation(display, LV_DISP_ROT_270);
 #endif
 
-    disp_drv.buffer = &disp_buf;
-    lv_disp_drv_register(&disp_drv);
-
-     /* Create and start a periodic timer interrupt to call lv_tick_inc */
+    /* Create and start a periodic timer interrupt to call lv_tick_inc */
     const esp_timer_create_args_t periodic_timer_args = {
         .callback = &lv_tick_task,
-        .name = "periodic_gui"
-    };
+        .name = "periodic_gui"};
     esp_timer_handle_t periodic_timer;
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
     /* Create the demo application */
+#if LV_USE_DEMO_WIDGETS
+    lv_demo_widgets();
+#else
     create_ui_application();
+#endif    
 
-    while (1) {
+    while (1)
+    {
         /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
         vTaskDelay(pdMS_TO_TICKS(10));
 
         /* Try to take the semaphore, call lvgl related function on success */
-        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY))
+        {
             lv_task_handler();
             xSemaphoreGive(xGuiSemaphore);
-       }
+        }
     }
 
     /* A task should NEVER return */
@@ -105,39 +123,39 @@ static void guiTask(void *pvParameter) {
     vTaskDelete(NULL);
 }
 
-
-static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
+static void btn_event_cb(lv_obj_t *btn, lv_event_t event)
 {
-    if(event == LV_EVENT_CLICKED) {
+    if (event.code == LV_EVENT_CLICKED)
+    {
         static uint8_t cnt = 0;
         cnt++;
 
         /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t * label = lv_obj_get_child(btn, NULL);
+        lv_obj_t *label = lv_obj_get_child(btn, NULL);
         lv_label_set_text_fmt(label, "Button: %d", cnt);
     }
 }
 
 static void create_ui_application(void)
 {
-    lv_obj_t * btn = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_t *btn = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                 /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);               /*Set its size*/
     // lv_obj_set_event_cb(btn, btn_event_cb);                 /*Assign a callback to the button*/
-    lv_obj_t * label = lv_label_create(btn, NULL);          /*Add a label to the button*/
-    lv_label_set_text(label, "Yeah");                     /*Set the labels text*/
+    lv_obj_t *label = lv_label_create(btn); /*Add a label to the button*/
+    lv_label_set_text(label, "Yeah");       /*Set the labels text*/
 
-    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
-    lv_obj_set_pos(btn2, 150, 10);                            /*Set its position*/
-    lv_obj_set_size(btn2, 120, 50);                          /*Set its size*/
-    lv_obj_t * label2 = lv_label_create(btn2, NULL);          /*Add a label to the button*/
-    lv_label_set_text(label2, "Buddy");                     /*Set the labels text*/
-
-
+    lv_obj_t *btn2 = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
+    lv_obj_set_pos(btn2, 150, 10);                /*Set its position*/
+    lv_obj_set_size(btn2, 120, 50);               /*Set its size*/
+    lv_obj_t *label2 = lv_label_create(btn2);     /*Add a label to the button*/
+    lv_label_set_text(label2, "Buddy");           /*Set the labels text*/
+    // ui_init();
 }
 
-static void lv_tick_task(void *arg) {
-    (void) arg;
+static void lv_tick_task(void *arg)
+{
+    (void)arg;
 
     lv_tick_inc(LV_TICK_PERIOD_MS);
 }
