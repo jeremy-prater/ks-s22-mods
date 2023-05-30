c7fb19c9deb845b16ec95a4bf6efa4771c029db0
Jeremy Prater <silverevo03@gmail.com>
2023-05-30 00:17:20 -0700

    New UI

--- s22_display/main/s22_display.c
+++ s22_display/main/s22_display.c
@@ -81,7 +81,9 @@ static void guiTask(void *pvParameter)
     disp_drv.draw_buf = &disp_buf;
     lv_disp_t *display = lv_disp_drv_register(&disp_drv);
 
-#if defined CONFIG_DISPLAY_ORIENTATION_LANDSCAPE
+#if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT
+    lv_disp_set_rotation(display, LV_DISP_ROT_NONE);
+#elif defined CONFIG_DISPLAY_ORIENTATION_LANDSCAPE
     lv_disp_set_rotation(display, LV_DISP_ROT_90);
 #elif defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
     lv_disp_set_rotation(display, LV_DISP_ROT_180);
@@ -131,26 +133,26 @@ static void btn_event_cb(lv_obj_t *btn, lv_event_t event)
         cnt++;
 
         /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t *label = lv_obj_get_child(btn, NULL);
+        lv_obj_t *label = lv_obj_get_child(btn, 0);
         lv_label_set_text_fmt(label, "Button: %d", cnt);
     }
 }
 
 static void create_ui_application(void)
 {
-    lv_obj_t *btn = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                 /*Set its position*/
-    lv_obj_set_size(btn, 120, 50);               /*Set its size*/
-    // lv_obj_set_event_cb(btn, btn_event_cb);                 /*Assign a callback to the button*/
-    lv_obj_t *label = lv_label_create(btn); /*Add a label to the button*/
-    lv_label_set_text(label, "Yeah");       /*Set the labels text*/
+    // lv_obj_t *btn = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
+    // lv_obj_set_pos(btn, 10, 10);                 /*Set its position*/
+    // lv_obj_set_size(btn, 120, 50);               /*Set its size*/
+    // // lv_obj_set_event_cb(btn, btn_event_cb);                 /*Assign a callback to the button*/
+    // lv_obj_t *label = lv_label_create(btn); /*Add a label to the button*/
+    // lv_label_set_text(label, "Yeah");       /*Set the labels text*/
 
-    lv_obj_t *btn2 = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
-    lv_obj_set_pos(btn2, 150, 10);                /*Set its position*/
-    lv_obj_set_size(btn2, 120, 50);               /*Set its size*/
-    lv_obj_t *label2 = lv_label_create(btn2);     /*Add a label to the button*/
-    lv_label_set_text(label2, "Buddy");           /*Set the labels text*/
-    // ui_init();
+    // lv_obj_t *btn2 = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
+    // lv_obj_set_pos(btn2, 150, 10);                /*Set its position*/
+    // lv_obj_set_size(btn2, 120, 50);               /*Set its size*/
+    // lv_obj_t *label2 = lv_label_create(btn2);     /*Add a label to the button*/
+    // lv_label_set_text(label2, "Buddy");           /*Set the labels text*/
+    ui_init();
 }
 
 static void lv_tick_task(void *arg)
